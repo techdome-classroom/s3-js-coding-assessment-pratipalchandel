4fe68eeb3b5afa9c1bdd70df071b218f51a58a6c
Pratipal Singh <142249985+pratipalchandel@users.noreply.github.com>
2024-10-07 15:28:21 +0530

    program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const bracket = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (bracket[char]) {
+            const topElement = stack.length ? stack.pop() : '#';
+            if (bracket[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
